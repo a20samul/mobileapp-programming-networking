f3ae8d534692c4bbcb0e8d89c02ac5cfb1d19079
a20samul <a20samul@student.his.se>
2021-04-27 10:54:59 +0200

    Added code for ArrayList, ArrayAdapter and created its member class

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,16 +3,37 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Mountain> items;
+    private ArrayAdapter<Mountain> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter(this, R.layout.listview_item, items);
+
         ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        //adapter = new ArrayAdapter<Mountain>(this,R.layout.(item, mountains);
+
 
     }
 }
